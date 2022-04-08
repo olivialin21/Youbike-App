d116cd020beb4d4f7beaebccc0e620d86dd44d17
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-04-08 16:37:13 +0800

    reload nodemodule

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+import { Provider } from "react-redux";
+import { store } from './src/redux/store';
 
-export default function App() {
+import Navigation from './src/navigation';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <NativeBaseProvider>
+          <Navigation />
+        </NativeBaseProvider>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
