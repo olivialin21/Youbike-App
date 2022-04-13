7dd5cba8a5aa75a80c5cf9f38beedd982d1fd3f9
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-04-13 12:28:34 +0800

    rentScreen & returnScreen map same place

--- src/redux/store/mapReducer.js
+++ src/redux/store/mapReducer.js
@@ -14,6 +14,7 @@ const initialState = {
 export const mapReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_REGION:
+      console.log(action.payload)
       return {
         ...state,
         region: { ...action.payload }
--- src/screens/RentScreen.js
+++ src/screens/RentScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from '@react-navigation/native';
 import { Box } from "native-base";
 import MapView from 'react-native-maps';
@@ -8,20 +8,24 @@ import { setRegion } from "../redux/actions/mapActions";
 const RentScreen = ({ navigation }) => {
   const dispatch = useDispatch();
   const region = useSelector((state) => (state.map.region));
-  console.log(region)
+  const [regionNow, setRegionNow] = useState(region);
+  useEffect (() => {
+    setRegionNow(region)
+  },[region])
   const onRegionChangeComplete = (rgn) => {
     if (
       Math.abs(rgn.latitude - region.latitude) > 0.0002 ||
       Math.abs(rgn.longitude - region.longitude) > 0.0002
     ) {
       dispatch(setRegion(rgn));
+      setRegionNow(rgn);
     }
   };
 
   return (
     <Box flex={1}>
       <MapView
-        region={region}
+        region={regionNow}
         flex={1}
         provider="google"
         onRegionChangeComplete={onRegionChangeComplete}
